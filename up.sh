e33dff5be557d06ad07f2683dd4e43cd58e0cc63
SupahXYT <josuefirelol@gmail.com>
2025-07-31 07:05:46 +0000

    asdf

--- up.sh
+++ up.sh
@@ -5,5 +5,8 @@
 
 export SERVER_IP=$(getent hosts $SERVER_HOSTNAME | awk '{print $1}')
 
-podman-compose -f ./compose/actual-compose.yml up -d
-podman-compose -f ./compose/baikal-compose.yml up -d
+printf "%s is: %s\n" "$SERVER_HOSTNAME" "$SERVER_IP"
+sudo printf "sudo: %s is: %s\n" "$SERVER_HOSTNAME" "$SERVER_IP"
+
+sudo -E podman-compose -f ./compose/actual-compose.yml up -d
+sudo -E podman-compose -f ./compose/baikal-compose.yml up -d
